b808ee0b95981e7bca32ae1d133fdf47e69aedc2
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2022-03-30 22:34:55 +0530

    finish building player

---
+++ src/components/JinkePlayer.jsx
@@ -0,0 +1,10 @@
+import ReactJkMusicPlayer from 'react-jinke-music-player'
+import 'react-jinke-music-player/assets/index.css'
+
+export const JinkePlayer = ()=>{
+    return <>
+    
+    <ReactJkMusicPlayer />
+
+    </>
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,79 @@
 import jioIcon from "../Assets/jioIcon.jpeg";
 import { BasicMenu } from "./MusicLanMenu.jsx";
+import Player from "react-material-music-player";
+import { Track, PlayerInterface } from "react-material-music-player";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+// console.log(uuid())
 
 export const Navbar = () => {
+  const [text, setText] = useState("");
+  const [response, setResponse] = useState([]);
+
+  // PlayerInterface.play([
+  //   new Track(
+  //     "1",
+  //     "",
+  //     "Arijit Singh",
+  //     "Arijit Singh",
+  //     "http://h.saavncdn.com/987/cd902d048c13e5ce6ca84cc409746a5d.mp3"
+  //  )
+  // ])
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      //make network request;
+      getSong(text);
+    }
+  };
+
+ 
+  if(response.length > 0){
+    PlayerInterface.play([
+      new Track(
+        response[0].id,
+        response[0].image,
+        response[0].album,
+        "arijit",
+        response[0].media_url
+      ),
+    ]);
+  
+    PlayerInterface.playLater([
+      new Track(
+        response[1].id,
+        response[1].image,
+        response[1].album,
+        "arijit",
+        response[1].media_url
+      ),
+    ]);
+  
+    PlayerInterface.playNext([
+      new Track(
+        response[2].id,
+        response[2].image,
+        response[2].album,
+        "arijit",
+        response[2].media_url
+      ),
+    ]);
+  }
+
+  
+
+  const getSong = (name) => {
+    axios
+      .get(`https://apg-saavn-api.herokuapp.com/result/?q=${name}&_limit=3`)
+      .then((res) => setResponse(res.data))
+        
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
       <div className="jioNavbar">
@@ -10,10 +81,19 @@ export const Navbar = () => {
         <p>Home</p>
         <p>Browse</p>
         <p>Upgrade</p>
-        <input className="searchBox" type="text" placeholder="Search" />
-        < BasicMenu  className="menuComponent"/>
+        <input
+          className="searchBox"
+          type="text"
+          placeholder="Search"
+          onKeyDown={(e) => {
+            handleKeyDown(e);
+          }}
+          onChange={handleChange}
+        />
+        <BasicMenu className="menuComponent" />
         <p>Login</p>
         <p>Sign Up</p>
+        <Player />
       </div>
     </>
   );
